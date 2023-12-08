b09e928ec3705109da64229129b18ee461b6cc19
asrikum2 <asrikum2@uwo.ca>
2023-12-07 23:08:29 -0500

    Final Code Implementation

--- client/src/loginbase/client/Signup/index.jsx
+++ client/src/loginbase/client/Signup/index.jsx
@@ -42,7 +42,7 @@ const Signup = () => {
 					<h1>Welcome Back</h1>
 					<Link to="/login">
 						<button type="button" className={styles.white_btn}>
-							Sing in
+							Sign in
 						</button>
 					</Link>
 				</div>
@@ -97,7 +97,7 @@ const Signup = () => {
 						{error && <div className={styles.error_msg}>{error}</div>}
 						{msg && <div className={styles.success_msg}>{msg}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sing Up
+							Sign Up
 						</button>
 					</form>
 				</div>
